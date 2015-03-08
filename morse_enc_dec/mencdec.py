74ee1731d249feb13124b5565d60a533e8f61342
dragos-constantin-stoica <dragos.constantin.stoica@yahoo.com>
2015-03-08 16:37:16 +0200

    Reference issue #15

--- morse_enc_dec/mencdec.py
+++ morse_enc_dec/mencdec.py
@@ -1,4 +1,5 @@
 import sys
+import unittest
 
 CODE = {'A': '.-',     'B': '-...',   'C': '-.-.', 
         'D': '-..',    'E': '.',      'F': '..-.',
@@ -13,26 +14,66 @@ CODE = {'A': '.-',     'B': '-...',   'C': '-.-.',
         '0': '-----',  '1': '.----',  '2': '..---',
         '3': '...--',  '4': '....-',  '5': '.....',
         '6': '-....',  '7': '--...',  '8': '---..',
-        '9': '----.' 
+        '9': '----.' ,
+        
+        ' ':'$'
         }
+
+
+inverseMorseAlphabet=dict((v,k) for (k,v) in CODE.items())
 
 def verify(string):
     keys = CODE.keys()
     for char in string:
-        if char.upper() not in keys and char != ' ':
-            sys.exit('Error the charcter ' + char + ' cannot be translated to Morse Code')
+        if char.upper() not in keys:
+           return False
+    return True
+
+
+# parse a morse code string positionInString is the starting point for decoding
+def decodeMorse(code, positionInString = 0):
+	if positionInString < len(code):
+		morseLetter = ""
+		for key,char in enumerate(code[positionInString:]):
+			if char == " ":
+				positionInString = key + positionInString + 1
+				letter = inverseMorseAlphabet[morseLetter]
+				return letter + decodeMorse(code, positionInString)
+			else:
+				morseLetter += char
+	else:
+		return ""
+
+#encode a message in morse code, spaces between words are represented by '/'
+def encodeToMorse(message):
+	encodedMessage = ""
+	for char in message[:]:
+		encodedMessage += CODE[char.upper()] + " "
+	return encodedMessage 
 
 def main():
     
     msg = raw_input('MESSAGE: ')
     
-    verify(msg)
+    if not verify(msg):
+		sys.exit('Error message cannot be translated to Morse Code')
+
+    print encodeToMorse(msg)
+
+class MyTest(unittest.TestCase):
+    # Ref issue #9
+    def test_toMorse(self):
+        self.assertMultiLineEqual(encodeToMorse('SOS'), '... --- ... ')
     
-    for char in msg:
-        if char == ' ':
-            print ' '*7,
-        else:
-            print CODE[char.upper()],
+    # Ref issue #9
+    def test_toASCII(self):
+		self.assertMultiLineEqual(decodeMorse('... --- ... $ .-.. . $ -- --- -. -.. . '), 'SOS LE MONDE')
+    
+    def test_verify(self):
+        self.assertFalse(verify(';'))
+        self.assertTrue(verify('SOS LE MONDE'))
+
         
 if __name__ == "__main__":
     main()
+    #unittest.main()
